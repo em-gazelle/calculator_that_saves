bd3e844f058443e10a9da16954d74a59a14626de
em-gazelle <emmaf9@gmail.com>
2018-04-13 13:04:37 -0400

    Slight Styling

--- app/javascript/components/Calculator.jsx
+++ app/javascript/components/Calculator.jsx
@@ -1,9 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-// import CalculatorButton from '../common/CalculatorButton'
-
-
 class Calculator extends React.Component {
   
   constructor(props) {
@@ -78,57 +75,50 @@ class Calculator extends React.Component {
 
     return (
 	  <div>
-	  	<React.Fragment>Calculate!</React.Fragment>
-	  	<div>
-		  <form onSubmit={this.handleSubmit}>
-	        <label>
-	          <input type="text" value={this.state.newCalculationQuery} onChange={this.handleQueryChange} />
-	        </label>
-	      </form>
-        </div>
+	  	<h3 className="center-text">~Calculate and Save~</h3>
 
         <div className="calculator">
-	        <div className="calculator-row">
-		        <button value="(" onClick={this.handleButtonQueryChanges} >&#40;</button>
-		        <button value=")" onClick={this.handleButtonQueryChanges} >&#41;</button>
-		        <button onClick={this.deleteCharacter} >&larr;</button>
-		        <button onClick={this.clearQueryForm} >~ AC ~</button>
+	        <div>
+	        	<input className="calculator-screen" type="text" value={this.state.newCalculationQuery} onChange={this.handleQueryChange} />
 	        </div>
-	        <div className="calculator-row">
-		        <button value="7" onClick={this.handleButtonQueryChanges} >7</button>
-		        <button value="8" onClick={this.handleButtonQueryChanges} >8</button>
-		        <button value="9" onClick={this.handleButtonQueryChanges} >9</button>
-		        <button value="sqrt" onClick={this.handleButtonQueryChanges} >&#8730;</button>
-		        <button value="^" onClick={this.handleButtonQueryChanges} >x<sup>a</sup></button>
+	        <div>
+		        <button className="small-btn btn-text" value="(" onClick={this.handleButtonQueryChanges} >&#40;</button>
+		        <button className="small-btn btn-text" value=")" onClick={this.handleButtonQueryChanges} >&#41;</button>
+		        <button className="small-btn btn-text" onClick={this.deleteCharacter} >&larr;</button>
+		        <button className="medium-btn btn-text" onClick={this.clearQueryForm} >AC</button>
 	        </div>
-	        <div className="calculator-row">
-		        <button value="4" onClick={this.handleButtonQueryChanges} >4</button>
-		        <button value="5" onClick={this.handleButtonQueryChanges} >5</button>
-		        <button value="6" onClick={this.handleButtonQueryChanges} >6</button>
-		        <button value="*" onClick={this.handleButtonQueryChanges} >&times;</button>
-		        <button value="/" onClick={this.handleButtonQueryChanges} >&divide;</button>
+	        <div>
+		        <button className="small-btn btn-text" value="7" onClick={this.handleButtonQueryChanges} >7</button>
+		        <button className="small-btn btn-text" value="8" onClick={this.handleButtonQueryChanges} >8</button>
+		        <button className="small-btn btn-text" value="9" onClick={this.handleButtonQueryChanges} >9</button>
+		        <button className="small-btn btn-text" value="sqrt" onClick={this.handleButtonQueryChanges} >&#8730;</button>
+		        <button className="small-btn btn-text" value="^" onClick={this.handleButtonQueryChanges} >x<sup>a</sup></button>
 	        </div>
-	        <div className="calculator-row">
-		        <button value="1" onClick={this.handleButtonQueryChanges} >1</button>
-		        <button value="2" onClick={this.handleButtonQueryChanges} >2</button>
-		        <button value="3" onClick={this.handleButtonQueryChanges} >3</button>
-		        <button value="+" onClick={this.handleButtonQueryChanges} >+</button>
-		        <button value="-" onClick={this.handleButtonQueryChanges} >-</button>
+	        <div>
+		        <button className="small-btn btn-text" value="4" onClick={this.handleButtonQueryChanges} >4</button>
+		        <button className="small-btn btn-text" value="5" onClick={this.handleButtonQueryChanges} >5</button>
+		        <button className="small-btn btn-text" value="6" onClick={this.handleButtonQueryChanges} >6</button>
+		        <button className="small-btn btn-text" value="*" onClick={this.handleButtonQueryChanges} >&times;</button>
+		        <button className="small-btn btn-text" value="/" onClick={this.handleButtonQueryChanges} >&divide;</button>
 	        </div>
-	        <div className="calculator-row">
-		        <button value="0" onClick={this.handleButtonQueryChanges} >0</button>
-		        <button value="." onClick={this.handleButtonQueryChanges} >.</button>
-		        <button onClick={this.handleSubmit} > &#61;&#61;&#61;&#61;&#61;&#61;&#61; </button>
+	        <div>
+		        <button className="small-btn btn-text" value="1" onClick={this.handleButtonQueryChanges} >1</button>
+		        <button className="small-btn btn-text" value="2" onClick={this.handleButtonQueryChanges} >2</button>
+		        <button className="small-btn btn-text" value="3" onClick={this.handleButtonQueryChanges} >3</button>
+		        <button className="small-btn btn-text" value="+" onClick={this.handleButtonQueryChanges} >+</button>
+		        <button className="small-btn btn-text" value="-" onClick={this.handleButtonQueryChanges} >-</button>
+	        </div>
+	        <div>
+		        <button className="small-btn btn-text" value="0" onClick={this.handleButtonQueryChanges} >0</button>
+		        <button className="small-btn btn-text" value="." onClick={this.handleButtonQueryChanges} >.</button>
+		        <button className="large-btn btn-text" onClick={this.handleSubmit} >&#65309;</button>
 	        </div>
         </div>
 
         <br/>
-        <div>
-        	Prior Calculations: 
-
-        	<ol>
-        		{calculations}
-        	</ol>
+        <div className="horizontally-centered">
+        	<h4 className="center-text">Prior Calculations:</h4>
+        	<ol>{calculations}</ol>
         </div>
       </div>
 
