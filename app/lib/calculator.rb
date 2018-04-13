70d96f8c2d0a1579239e13a4046dff7132e22409
em-gazelle <emmaf9@gmail.com>
2018-04-12 20:03:58 -0400

    Calculator Module and Spec

--- app/lib/calculator.rb
+++ app/lib/calculator.rb
@@ -1,3 +1,154 @@
 module Calculator
 
+	def self.calculate_and_format(calculation_query)
+		"#{calculation_query} = #{calculate(calculation_query)}"
+	end
+
+	def self.calculate(expression)
+		return invalid_query if expression.include?(invalid_query)
+		
+		expression = remove_invalid_characters_and_separate_numbers_and_operators(expression) if expression.is_a?(String)
+		total = expression.count
+
+		if total.zero? 
+			expression
+		elsif total == 1
+			expression.first.is_a?(Float) ? expression.first : invalid_query 
+		elsif total == 2
+			if !expression.last.is_a?(Float)
+				invalid_query
+			elsif ["+", "-"].include?(expression[0])
+				merge_sign_into_float(expression[0], expression[1])
+			elsif expression[0] == "sqrt"
+				perform_individual_calculation(expression[0], expression[1])
+			else
+				invalid_query
+			end
+		elsif total >= 2
+			# recursion .. sending to perf_ind_calc but calling calculate on substrings (within parens)
+			# order of operations
+			if expression.index(")")
+				closing_parenthesis_index = expression.index(")")
+				opening_parenthesis_index = expression[0...closing_parenthesis_index].rindex("(")
+
+				if ((closing_parenthesis_index-opening_parenthesis_index) > 1) 
+					innermost_parenthesis_expression = expression[opening_parenthesis_index+1..closing_parenthesis_index-1]
+					inner_parentheses_calculated = [calculate(innermost_parenthesis_expression)]
+				else
+					inner_parentheses_calculated = []
+				end
+
+				expression = ( opening_parenthesis_index.zero? ? [] : expression[0..(opening_parenthesis_index-1)] ) + inner_parentheses_calculated + ( (closing_parenthesis_index==total) ? [] : expression[closing_parenthesis_index+1..-1] )
+			else
+				if expression.rindex("sqrt")
+					operator_index = expression.rindex("sqrt")
+					expression[operator_index+1..-1] = next_float(expression[operator_index+1..-1]) if !expression[operator_index+1].is_a?(Float)
+					expression[operator_index..operator_index+1] = [perform_individual_calculation("sqrt", expression[operator_index+1])]
+				elsif expression[0].is_a?(String)
+					if ["+", "-"].include?(expression[0]) && expression[1].is_a?(Float)
+						expression[0..1] = merge_sign_into_float(expression[0], expression[1])
+					else
+						invalid_query
+					end
+				elsif expression.index("^")
+					operator_index = expression.index("^")
+					invalid_query if !(expression[operator_index-1]).is_a?(Float)
+					expression[operator_index+1..-1] = next_float(expression[operator_index+1..-1]) if !expression[operator_index+1].is_a?(Float)
+
+					expression[operator_index-1..operator_index+1] = [perform_individual_calculation(expression[operator_index-1], "^", expression[operator_index+1])]
+				elsif expression.index("*") || expression.index("/")
+					operator_index = expression.index("*") || expression.index("/")				
+
+					invalid_query if !(expression[operator_index-1]).is_a?(Float)
+					expression[operator_index+1..-1] = next_float(expression[operator_index+1..-1]) if !expression[operator_index+1].is_a?(Float)
+
+					expression[operator_index-1..operator_index+1] = [perform_individual_calculation(expression[operator_index-1], expression[operator_index], expression[operator_index+1])]
+				elsif expression.index("+") || expression.index("-")
+					operator_index = expression.index("+") || expression.index("-")	
+
+					if operator_index == total
+						invalid_query
+					elsif !(expression[operator_index-1]).is_a?(Float)
+						if ["+", "-"].include?(expression[operator_index-1])
+							handle_two_add_subtr(operator_index-1, operator_index)
+						else
+							invalid_query
+						end
+					elsif !expression[operator_index+1].is_a?(Float)
+						expression[operator_index+1..-1] = next_float(expression[operator_index+1..-1]) if !expression[operator_index+1].is_a?(Float)
+					else
+						expression[operator_index-1..operator_index+1] = [perform_individual_calculation(expression[operator_index-1], expression[operator_index], expression[operator_index+1])]
+					end
+				else
+					expression = invalid_query
+				end
+			end
+			calculate(expression) 
+		end
+	end
+
+	def self.next_float(sub_expression)
+		until sub_expression.first.is_a?(Float) || sub_expression = invalid_query do
+
+			sub_expression.each_with_index do |c, i|
+				if ["+", "-"].include?(c) && ["+", "-"].include?(expression[i+1])
+					expression[i..i+1] = handle_two_add_subtr([c, expression[i+1]])
+				elsif ["+", "-"].include?(c) && expression[i+1].is_a?(Float)
+					expression[i..i+1] = merge_sign_into_float(c, expression[i+1])
+				else
+					invalid_query
+				end			
+			end
+		end
+		sub_expression
+	end
+
+	def self.perform_individual_calculation(first_number, operator, second_number=nil)
+		# process_multi_negatives(operator) if (operator.is_a?(String) && operator.include?("-") && operator.length > 1)
+		# can assume at least one of these numbers is an operator
+
+		if second_number
+			if ["+", "-", "*", "/"].include?(operator)
+				(first_number).send(operator, second_number)
+			elsif operator == "^"
+				first_number**second_number
+			else
+				invalid_query
+			end
+		elsif (first_number == "sqrt") && operator.is_a?(Float)
+			Math.sqrt(operator)
+		else
+			invalid_query
+		end
+	end
+
+	def self.merge_sign_into_float(sign, float)
+		if float < 0 && sign == "-"
+			"#{float*-1}".to_f
+		elsif float < 0 && sign == "+"
+			float
+		else
+			"#{sign}#{float}".to_f
+		end
+	end
+
+	def self.invalid_query
+		expression = "Invalid Query"
+	end
+
+	def self.remove_invalid_characters_and_separate_numbers_and_operators(expression)
+		# supporting decimals (.9, 0.9, 9.9, 99) 
+		valid_characters_only = expression.scan(/\+|\*|\/|-+|\^|sqrt|\(|\)|\d+\.\d*|\.\d+|\d+/)
+		array_with_floats = valid_characters_only.map{|c| c.match(/\d+/) ? c.to_f : c }
+		array_with_floats_and_no_double_negatives = array_with_floats.map{|c| c.is_a?(String) && c.include?("--") ? process_multi_negatives(c) : c}
+	end
+
+	def self.process_multi_negatives(negative_subtraction)
+		negative_subtraction.length.even? ? "+" : "-"
+	end
+
+	def self.handle_two_add_subtr(array)
+		(array.uniq.count == 1) ? ["-"] : ["+"]
+	end
+
 end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe Calculator do
 			context 'simple and valid' do
 				it 'does not throw error if no calculation to perform, but valid number in query' do
 					expect(Calculator.calculate("-33328282.9")).to eq(-33328282.9)
-					expect(Calculator.calculate("+5+3")).to eq(8.0)
 					expect(Calculator.calculate(".98789")).to eq(0.98789)
 					expect(Calculator.calculate("0")).to eq(0)
+					expect(Calculator.calculate("4()")).to eq(4.0)
 				end
 				it 'converts string to integers, operators, and returns proper calculation' do
 					expect(Calculator.calculate("3+4")).to eq(7)
+					expect(Calculator.calculate("+5+3")).to eq(8.0)
 				end
 				it 'handles decimals, fractions, and non-integer division results' do
 					expect(Calculator.calculate("3/4")).to eq(0.75)
@@ -19,23 +20,27 @@ RSpec.describe Calculator do
 					expect(Calculator.calculate("(2/3)/(3/2)").zero?).to be false
 				end
 				it "handles exponent sign '^' as Ruby's '**" do
+					expect(Calculator.calculate("3^3*2")).to eq(54)
+					expect(Calculator.calculate("-(3^3)*2")).to eq(-54)
 					expect(Calculator.calculate("(3^3)*2")).to eq(54)
 				end
 				it 'handles negative and subtraction signs properly' do
-					expect(Calculator.calculate("3----4")).to eq(-1)
+					expect(Calculator.calculate("3----4")).to eq(7)
 					expect(Calculator.calculate("-3+4*(4-4)")).to eq(-3)
 				end
 				it "converts 'sqrt' input find square root" do
 					expect(Calculator.calculate("sqrt(4)")).to eq(2)
 					expect(Calculator.calculate("sqrt4")).to eq(2)
+					expect(Calculator.calculate("sqrtsqrt16")).to eq(2)
+					expect(Calculator.calculate("-sqrtsqrt(2*9-2)")).to eq(-2)
 					expect(Calculator.calculate("sqrt(-(2-6))")).to eq(2)
 				end
 			end
 		end
 		context 'when invalid characters are included' do
 			it 'deletes invalid characters and returns calculation if otherwise valid' do
-				expect(Calculator.calculate("a3a3a+3a b-d3!?""")).to eq(33)
-				expect(Calculator.calculate("a3a3a+aaabdklsqrt4a")).to eq(35)
+				expect(Calculator.calculate("a3a+3a b-d3!?""")).to eq(3)
+				expect(Calculator.calculate("a33a+aaabdklsqrt4a")).to eq(35)
 			end
 			it 'returns error if query still invalid after invalid characters deleted' do
 				expect(Calculator.calculate("fnfn4abfch-j3j+")).to eq("Invalid Query")
@@ -44,7 +49,8 @@ RSpec.describe Calculator do
 		end
 		context 'when invalid query' do
 			it 'returns query string with error message stating that query was invalid' do
-				expect(Calculator.calculate("5+3-")).to eq("Invalid Query")				
+				expect(Calculator.calculate("5+3-")).to eq("Invalid Query")		
+				expect(Calculator.calculate("sqrt--sqrt16")).to eq("Invalid Query")		
 			end
 			it 'does not return imaginary numbers' do
 				expect(Calculator.calculate("sqrt-5")).to eq("Invalid Query")				
@@ -55,6 +61,9 @@ RSpec.describe Calculator do
 	describe 'remove_invalid_characters_and_separate_numbers_and_operators' do
 		it 'accurately deletes all invalid characters, supports operators (sqrt, (), ^, *, -----, +, /), and decimals. Converts all non-operators to floats, returns as array' do
 			expect(Calculator.remove_invalid_characters_and_separate_numbers_and_operators("djdjjd8888+++000_-    3*35.9+.9^sqrt(3.3.3)")).to eq([8888.0, "+", "+", "+", 0.0, "-", 3.0, "*", 35.9, "+", 0.9, "^", "sqrt", "(", 3.3, 0.3, ")"])
+		end
+		it 'deletes invalid characters, separates numbers that have invalid characters between' do
+			expect(Calculator.remove_invalid_characters_and_separate_numbers_and_operators("a3a3a+3a b-d3!?""")).to eq([3.0, 3.0, "+", 3.0, "-", 3.0])		
 		end
 	end
 
